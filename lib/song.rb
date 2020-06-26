2d89e927e9ef2cbf75c013dab8451d7e6485eb4d
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 16:00:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+   @@count = 0
+   @@artists = []
+   @@genre = []
+   @@name = name
+  
+  def initialize(name, artist, genre)
+    @@name = name
+    @@count +=1
+    @@genre = genre
+  end
+  
+    def self.artist
+      @@artist
+    end
+
+    def self.count
+      @@count
+    end
+
+    def self.name
+      @@name
+    end 
+
+    def self.genre
+    @@genre.uniq
+    end
+
+end
