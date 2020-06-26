8291e8a9d1483a5d1c262a26b2ceee3408600db7
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 17:17:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,5 +30,17 @@ class Song
     def self.genre
     @@genre.uniq
     end
+    
+      def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 
 end
